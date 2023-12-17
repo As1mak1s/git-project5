3fe3ac728967ed5e8de27f33d01f0800510f191e
As. Sampanis <sampanis.asimakis@gmail.com>
2023-12-17 13:02:25 +0200

    Main update

--- src/gr/aueb/cf/project5/Main.java
+++ src/gr/aueb/cf/project5/Main.java
@@ -2,6 +2,6 @@ package gr.aueb.cf.project5;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello cf");
+        System.out.println("Hello cf!!!!");
     }
 }
